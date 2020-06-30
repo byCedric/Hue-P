152cb8b75b86c50efc65741910c10d56aae36800
Cedric van Putten <me@bycedric.com>
2020-06-30 22:06:25 +0200

    fix: ios background color for safe area views (#3)

--- src/components/screen.tsx
+++ src/components/screen.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
-import { Layout } from '@ui-kitten/components';
+import { Layout, useTheme } from '@ui-kitten/components';
 
-export const Screen: React.FC = (props) => (
-	<SafeAreaView style={styles.container}>
-		<Layout style={styles.container}>
-			{props.children}
-		</Layout>
-	</SafeAreaView>
-);
+export const Screen: React.FC = (props) => {
+	const theme = useTheme();
+	const bgStyle = { backgroundColor: theme['color-basic-800'] };
+
+	return (
+		<SafeAreaView style={[styles.container, bgStyle]}>
+			<Layout style={styles.container}>
+				{props.children}
+			</Layout>
+		</SafeAreaView>
+	);
+};
 
 const styles = StyleSheet.create({
 	container: {
